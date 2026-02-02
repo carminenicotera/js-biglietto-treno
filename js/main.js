e0192886a374ffede1f0ebe1151c737270152ce4
carminenicotera <carmine.91.n@hotmail.it>
2026-02-02 15:30:46 +0100

    add conditions

--- js/main.js
+++ js/main.js
@@ -5,3 +5,11 @@ const km = Number(prompt('Type km'));
 //Calcolo prezzo del biglietto in base ai km da percorrere
 let price = km * 0.21;
 
+//Eseguo i controlli condizionali per applicare l'eventuale sconto
+if (age < 18) {
+  let discount_minor = (price * 20) / 100;
+  price -= discount_minor;
+} else if (age > 65) {
+  let discount_senior = (price * 40) / 100;
+  price -= discount_senior;
+}
