ac7f207d39c98902356a972d1c8bcedbc182d832
carminenicotera <carmine.91.n@hotmail.it>
2026-02-02 15:16:55 +0100

    add variables

---
+++ js/main.js
@@ -0,0 +1,7 @@
+//Inserisci i dati tramite il prompt
+const age = Number(prompt('Type age'));
+const km = Number(prompt('Type km'));
+
+//Calcolo prezzo del biglietto in base ai km da percorrere
+let price = km * 0.21;
+
